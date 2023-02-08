0283a354efec68f50e1cb403b0ca305e0a8a8d41
Jun-Sang Yoo <junsang7777@korea.ac.kr>
2023-02-08 16:12:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,30 +15,24 @@ conf = configs.Config()
 res_dir = prepare_result_dir(conf)
 
 files = [file_path for file_path in glob.glob('./set/img/*.png')]
-#depths = [depth_path for depth_path in glob.glob('./set14_test/*.jpeg')]
-#files = [file_path for file_path in glob.glob('../RGB_OUT/*.png')]
 depths = [depth_path for depth_path in glob.glob('./set/dep/*.png')]
 files = sorted(files)
 depths = sorted(depths)
 
-Kernel_switch = False
-kernels_ = [kernel_path for kernel_path in glob.glob('./set/ker/*.mat')]
-kernels_ = sorted(kernels_)
-#gimgs = [g_path for g_path in glob.glob('../RGB/*.png')]
-#gimgs = sorted(gimgs)
+kernels_ = [kernel_path for kernel_path in glob.glob('./set/ker/*.mat')] # if don't have kernel, annot.
+kernels_ = sorted(kernels_) # if don't have kernel, annot.
+
 for file_ind, input_file in enumerate(files):
 
     conf.result_path = res_dir
 
     input_depth = depths[file_ind]
-    #input_depth = kkk + '/' + y + '_disp.jpeg'
-    kernels = kernels_[file_ind]
-    kernels = [kernels]
-    #g_img = gimgs[file_ind]
+    kernels = kernels_[file_ind]  # if don't have kernel, annot.
+    kernels = [kernels] # if don't have kernel, annot.
 
 
     print('input file : ', input_file)
     print('input depth : ', input_depth)
     print('input kernel : ', kernels)
-    net = ZSSR.ZSSR(input_file,input_depth=input_depth,conf=conf,kernels=kernels)
+    net = ZSSR.ZSSR(input_file,input_depth=input_depth,conf=conf,kernels=kernels)  # if don't have kernel, 'change kerenls = None'
     net.run()
